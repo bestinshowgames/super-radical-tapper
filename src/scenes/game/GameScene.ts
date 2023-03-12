acd38c52233c8aef7b25ffce4bfa537d284c00d9
the-camputer <mynichor@gmail.com>
2023-03-11 20:02:36 -0500

    gh-13 added streak counter; updated score increment to increase with streak

--- src/scenes/game/GameScene.ts
+++ src/scenes/game/GameScene.ts
@@ -8,6 +8,7 @@ import CueFacet from './CueFacet';
 export default class Game extends Scene {
   private resultText!: GameObjects.Text;
   private scoreText!: GameObjects.Text;
+  private streakText!: GameObjects.Text;
   private gm!: GameController;
 
   constructor() {
@@ -42,22 +43,28 @@ export default class Game extends Scene {
       .setOrigin(0.5);
 
     this.resultText = this.add
-      .text(400, 150, '', { font: '64px Toriko' })
+      .text(400, 200, '', { font: '64px Toriko' })
       .setOrigin(0.5);
 
     // Score Text with Label
     this.add.text(100, 100, 'Score:', { font: '64px Toriko' }).setOrigin(0.5);
-
     this.scoreText = this.add
-      .text(100, 150, '0', { font: '64px Toriko' })
+      .text(100, 150, this.gm.score.toString(), { font: '64px Toriko' })
+      .setOrigin(0.5);
+
+    this.add.text(400, 100, 'Streak:', { font: '64px Toriko' }).setOrigin(0.5);
+    this.streakText = this.add
+      .text(400, 150, this.gm.streak.toString(), { font: '64px Toriko' })
       .setOrigin(0.5);
 
     this.events.on('succeed', () => {
       this.scoreText.setText(this.gm.score.toString());
+      this.streakText.setText(this.gm.streak.toString());
       this.resultText.setText('SCORE!');
     });
 
     this.events.on('fail', () => {
+      this.streakText.setText(this.gm.streak.toString());
       this.resultText.setText('OUCH!');
     });
 
